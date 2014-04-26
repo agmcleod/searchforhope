95a1192b0723475a0c861f50c5362e961bf5780a
Aaron McLeod <aaron.mcleod@mosaic.com>
2014-04-26 15:14:39 -0400

    better utilizing velocity & friction

--- js/components/dash.js
+++ js/components/dash.js
@@ -1,24 +1,12 @@
 game.dash = {
-  init: function(entity) {
+  dash: function(entity) {
     entity.dashing = true;
     var mousePos = me.game.viewport.localToWorld(me.input.mouse.pos.x, me.input.mouse.pos.y);
-    // v = p2 - p1; v = v:normalize() * min( v:len(), maxlength )
     entity.direction.set((mousePos.x - entity.pos.x), (mousePos.y - entity.pos.y));
     entity.direction.normalize();
     entity.direction.scaleV(entity.dashVel);
 
     entity.renderable.setCurrentAnimation('dash');
     entity.vel.add(entity.direction);
-    //entity.distanceTravelled.set(0, 0);
-  },
-
-  move: function(entity) {
-    entity.vel.add(entity.direction);
-    //entity.distanceTravelled.add(entity.direction);
-    // if(entity.distanceTravelled.x > 300 || entity.distanceTravelled.x < -300 || entity.distanceTravelled.y > 50 || entity.distanceTravelled.y < -50) {
-    //   entity.dashing = false;
-    //   this.falling = true;
-    //   entity.setDefaultAnimation();
-    // }
   }
 }
--- js/entities/player.js
+++ js/entities/player.js
@@ -1,7 +1,7 @@
 game.Player = me.ObjectEntity.extend({
   init: function(x, y, settings) {
     this._super(me.ObjectEntity, "init", [x, y, settings]);
-    this.setVelocity(3, 20);
+    this.setVel();
     this.getShape().resize(28, 32);
     this.getShape().translate(2, 0);
     this.renderable.addAnimation('dash', [0], 1);
@@ -11,9 +11,8 @@ game.Player = me.ObjectEntity.extend({
     this.health = 3;
     this.dashing = false;
     this.direction = new me.Vector2d();
-    this.dashVel = new me.Vector2d(300, 50);
-    this.distanceTravelled = new me.Vector2d();
-    this.setFriction(10, 0);
+    this.dashVel = new me.Vector2d(30, 30);
+    this.setFriction(2, 0);
   },
 
   damagedCallback: function() {
@@ -24,10 +23,12 @@ game.Player = me.ObjectEntity.extend({
     if (me.input.isKeyPressed('left')) {
       this.flipX(true);
       this.vel.x -= this.accel.x * me.timer.tick;
+      this.movementSetup();
     }
     else if (me.input.isKeyPressed('right')) {
       this.flipX(false);
       this.vel.x += this.accel.x * me.timer.tick;
+      this.movementSetup();
     }
     else if(!this.dashing) {
       this.vel.x = 0;
@@ -39,92 +40,30 @@ game.Player = me.ObjectEntity.extend({
     }
 
     if (me.input.isKeyPressed('dash') && !this.dashing && !this.falling) {
-      game.dash.init(this);
+      game.dash.dash(this);
+      this.setMaxVelocity(100, 50);
+      if(this.direction.x < 0) {
+        this.flipX(true);
+      }
+      else {
+        this.flipX(false);
+      }
     }
   },
 
-  moveByVelocity: function() {
-    this.computeVelocity(this.vel);
-
-    // Adjust position only on collidable object
-    var collision;
-    // save the collision box offset
-    this._bounds = this.getBounds(this._bounds);
-    this.__offsetX = this._bounds.pos.x;
-    this.__offsetY = this._bounds.pos.y;
-    this._bounds.translateV(this.pos);
-
-    // check for collision
-    collision = this.collisionMap.checkCollision(this._bounds, this.vel);
-
-    // update some flags
-    this.onslope  = collision.yprop.isSlope || collision.xprop.isSlope;
-    // clear the ladder flag
-    this.onladder = false;
-    var prop = collision.yprop;
-    var tile = collision.ytile;
-
-    // y collision
-    if (collision.y) {
-      // going down, collision with the floor
-      this.onladder = prop.isLadder || prop.isTopLadder;
-
-      if (collision.y > 0) {
-        if (prop.isSolid || (prop.isPlatform && (this._bounds.bottom - 1 <= tile.pos.y))) {
-          // adjust position to the corresponding tile
-          this._bounds.pos.y = ~~this._bounds.pos.y;
-          this.vel.y = 0;
-          this.falling = false;
-        }
-        else if (prop.isSlope && !this.jumping) {
-          // we stop falling
-          this.checkSlope(
-            this._bounds,
-            tile,
-            prop.isLeftSlope
-          );
-          this.falling = false;
-        }
-      }
-      // going up, collision with ceiling
-      else if (collision.y < 0) {
-        if (!prop.isPlatform && !prop.isLadder && !prop.isTopLadder) {
-          if (this.gravity) {
-              this.falling = true;
-          }
-          // cancel the y velocity
-          this.vel.y = 0;
-        }
-      }
+  movementSetup: function() {
+    this.setVel();
+    if(!this.renderable.isCurrentAnimation('run')) {
+      this.renderable.setCurrentAnimation('run');
     }
-    prop = collision.xprop;
-    tile = collision.xtile;
-
-    // x collision
-    if (collision.x) {
-      if (prop.isSlope && !this.jumping) {
-        this.checkSlope(this._bounds, tile, prop.isLeftSlope);
-        this.falling = false;
-      }
-      else {
-        // can walk through the platform
-        if (!prop.isPlatform) {
-          this.vel.x = 0;
-        }
-      }
-    }
-
-    this.pos.set(
-      this._bounds.pos.x - this.__offsetX,
-      this._bounds.pos.y - this.__offsetY
-    );
-
-    // update player position
-    this.pos.add(this.vel);
   },
 
   setDefaultAnimation: function() {
     this.renderable.setCurrentAnimation('run');
+  },
+
+  setVel: function() {
+    this.setMaxVelocity(3, 20);
   },
 
   update: function(time) {
@@ -150,21 +89,15 @@ game.Player = me.ObjectEntity.extend({
         this.setDefaultAnimation();
       }  
     }
-    
 
-    if (this.dashing) {
-      //game.dash.move(this);
-      //this.moveByVelocity();
-    }
-    //else {
-      this.updateMovement();
-    //}
+    this.updateMovement();
     
     if (this.vel.x !== 0 || this.vel.y !== 0) {
       this._super(me.ObjectEntity, 'update', [time]);
       return true;
     }
     else {
+      this.dashing = false;
       return false;
     }
   }
