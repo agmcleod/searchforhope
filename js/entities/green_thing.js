9cff79992867abcd36f595c2a9abef65ac79cb04
Aaron McLeod <sircoolguy99@gmail.com>
2014-08-25 23:20:54 -0400

    updatd level one with new collision

--- js/entities/green_thing.js
+++ js/entities/green_thing.js
@@ -7,12 +7,21 @@ game.GreenThing = me.Entity.extend({
     this.body.collisionType = me.collision.types.ENEMY_OBJECT;
   },
 
+  collideHandler: function (response) {
+    switch (response.b.body.collisionType) {
+      case me.collision.types.WORLD_SHAPE:
+        this.pos.sub(response.overlapV);
+        break;
+    }
+  },
+
   onCollision: function(res, obj) {
     game.enemy.onCollision(this, res, obj);
   },
 
   update: function(time) {
     this._super(me.Entity, 'update', [time]);
+    me.collision.check(this, true, this.collideHandler.bind(this), true);
     return true;
   }
 });
