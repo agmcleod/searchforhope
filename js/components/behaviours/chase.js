7490183f5758209e3818c6de9cb233dd7235ebe4
Aaron McLeod <sircoolguy99@gmail.com>
2014-10-11 22:57:00 -0400

    better stoppage

--- js/components/behaviours/chase.js
+++ js/components/behaviours/chase.js
@@ -1,16 +1,17 @@
 game.chaseBehaviour = {
   execute: function (entity, target) {
-    var diff = entity.pos.x - target.x;
-    if (diff > entity.maxChase || diff < -entity.maxChase) {
+    var diff = Math.abs(entity.pos.x - target.x);
+    var accelx = entity.body.accel.x * me.timer.tick;
+    if (diff > entity.maxChase || diff <= accelx) {
       return;
     }
     if (entity.pos.x > target.x) {
       entity.renderable.flipX(false);
-      entity.body.vel.x -= entity.body.accel.x * me.timer.tick;
+      entity.body.vel.x -= accelx;
     }
     else if (entity.pos.x < target.x) {
       entity.renderable.flipX(true);
-      entity.body.vel.x += entity.body.accel.x * me.timer.tick;
+      entity.body.vel.x += accelx;
     }
   }
 };
