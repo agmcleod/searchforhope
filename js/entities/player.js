e07bb4161b68dc1ee520f0a1fd6599731e6d6bc9
Aaron McLeod <sircoolguy99@gmail.com>
2014-10-21 22:18:43 -0400

    dashing kinda working, but needs some tweaking

--- js/entities/player.js
+++ js/entities/player.js
@@ -1,10 +1,10 @@
 game.Player = me.Entity.extend({
-  init: function(x, y, settings) {
+  init: function (x, y, settings) {
     settings.width = 32;
     settings.height = 32;
     this._super(me.Entity, "init", [x, y, settings]);
     this.body.setShape(2, 0, 24, 32);
-    this.body.setVelocity(5, 21);
+    this.setMovementVelocity();
     this.body.setFriction(1.5, 0);
     this.body.collisionType = me.collision.types.PLAYER_OBJECT;
 
@@ -19,7 +19,6 @@ game.Player = me.Entity.extend({
     this.health = game.playScreen.playerHealth;
     this.dashing = false;
     this.direction = new me.Vector2d();
-    this.dashVel = new me.Vector2d(5, 20);
 
     this.renderable.addAnimation('dash', [0], 1);
     this.renderable.addAnimation('run', [1,2,3,4,5,6,7,8,9], 20);
@@ -34,11 +33,11 @@ game.Player = me.Entity.extend({
     this.messageTwoSprite.offset.setV(messageTwoRegion.offset);
   },
 
-  damagedCallback: function() {
+  damagedCallback: function () {
     this.damaged = false;
   },
 
-  draw: function(renderer) {
+  draw: function (renderer) {
     this._super(me.Entity, 'draw', [renderer]);
     if(this.damaged) {
       game.playScreen.uiFont.draw(renderer.getContext(), this.health, this.pos.x - 10, this.pos.y - 10);
@@ -54,7 +53,7 @@ game.Player = me.Entity.extend({
     }
   },
 
-  handleInput: function() {
+  handleInput: function () {
     if (me.input.isKeyPressed('left')) {
       this.renderable.flipX(true);
       this.body.vel.x -= this.body.accel.x * me.timer.tick;
@@ -71,8 +70,6 @@ game.Player = me.Entity.extend({
     }
 
     if (me.input.isKeyPressed('jump')) {
-      this.jumping = true;
-
       this.jumpState = (this.body.vel.y === 0)?1:this.jumpState;
 
       if (this.jumpState <= 1) {
@@ -82,10 +79,11 @@ game.Player = me.Entity.extend({
 
     if (me.input.isKeyPressed('dash') && !this.dashing && !this.body.jumping) {
       this.dashing = true;
+      this.setDashVelocity();
       var mousePos = me.game.viewport.localToWorld(me.input.mouse.pos.x, me.input.mouse.pos.y);
       this.direction.set((mousePos.x - this.pos.x), (mousePos.y - this.pos.y));
       this.direction.normalize();
-      this.direction.scaleV(this.dashVel);
+      this.direction.scaleV(this.body.maxVel);
       this.renderable.setCurrentAnimation('dash');
       this.body.vel.add(this.direction);
       if(this.direction.x < 0) {
@@ -97,7 +95,7 @@ game.Player = me.Entity.extend({
     }
   },
 
-  movementSetup: function() {
+  movementSetup: function () {
     if(!this.renderable.isCurrentAnimation('run')) {
       this.renderable.setCurrentAnimation('run');
     }
@@ -112,7 +110,7 @@ game.Player = me.Entity.extend({
           game.playScreen.lowerHealth();
           if(this.health <= 0) {
             game.playScreen.resetHealth();
-            this.renderable.flicker(300, function() {
+            this.renderable.flicker(300, function () {
               me.levelDirector.loadLevel.defer(this, 'intro');
             });
           }
@@ -130,11 +128,19 @@ game.Player = me.Entity.extend({
     };
   },
 
-  setDefaultAnimation: function() {
+  setDashVelocity: function () {
+    this.body.setVelocity(22, 30);
+  },
+
+  setDefaultAnimation: function () {
     this.renderable.setCurrentAnimation('run');
   },
 
-  update: function(time) {
+  setMovementVelocity: function () {
+    this.body.setVelocity(5, 21);
+  },
+
+  update: function (time) {
     this.handleInput();
 
     this.body.update();
@@ -143,7 +149,7 @@ game.Player = me.Entity.extend({
     if (this.body.vel.x !== 0 || this.body.vel.y !== 0) {
       this._super(me.Entity, 'update', [time]);
     }
-    else {
+    else if(this.dashing) {
       this.dashing = false;
     }
     return true;
